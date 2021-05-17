350089c00fc722f1aa056b86e9664a81412d8610
Hee <kuncc@foxmail.com>
2021-05-17 23:05:58 +0800

    添加商城 一级页面跳转

---
+++ pages/mall/components/service.vue
@@ -0,0 +1,9 @@
+<template>
+	<view>产品服务</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- pages/mall/mall.vue
+++ pages/mall/mall.vue
@@ -1,22 +1,36 @@
 <template>
-	<view>
-		
+	<view class="mall">
+		<navigator url="./components/sunYuan"><view>笋园</view></navigator>
+		<navigator url="./components/service"><view>产品服务</view></navigator>
+		<navigator url="./components/liveBroadcast"><view>直播</view></navigator>
+		<view>热销产品 (卡片流)</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+	data() {
+		return {}
+	},
+	methods: {}
+}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.mall{
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+	margin-top: 100rpx;
+	navigator{
+		view{
+			border: 1px solid #1BBB5A;
+			padding: 50rpx;
+			border-radius: 20rpx;
+		}
+	}
+	> view{
+		margin-top: 100rpx;
+	}
+}
 </style>

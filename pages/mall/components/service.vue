64d6d8d2077acf0fe1ceda5657cd1abf45662e28
Hee <kuncc@foxmail.com>
2021-05-18 08:56:59 +0800

    service 页面可拖拉组件出问题

--- pages/mall/components/service.vue
+++ pages/mall/components/service.vue
@@ -1,9 +1,35 @@
 <template>
-	<view>产品服务</view>
+	<view class="service">
+		<view>共享村庄</view>
+		<view>共享竹林</view>
+		<movable-area>
+			<movable-item direction="all"><view></view></movable-item>
+		</movable-area>
+	</view>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style>
+<style lang="scss" scoped>
+.service {
+	movable-area {
+		width: 750rpx;
+		height: 100vh;
+		border: 1px solid blue;
+		movable-view{
+			width: 100rpx;
+			height: 100rpx;
+		}
+		view {
+			width: 100rpx;
+			height: 100rpx;
+			border: 1px solid red;
+			box-shadow: 0 0 1px 1px rgba(0, 0, 0, 0.25);
+			image{
+				width: 100rpx;
+				height: 100rpx;
+			}
+		}
+	}
+}
 </style>
